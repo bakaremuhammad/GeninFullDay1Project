0c4951a178bb8fd9726ca4a50e39aec09b69f462
bakaremuhammad <thepsychometric09@gmail.com>
2024-05-28 19:36:37 +0100

    Commit

---
+++ main.py
@@ -0,0 +1,54 @@
+#Genin Project
+#Day 1
+print("Hello World!")
+
+#Day1.1
+print("Day 1 - Python Print Function")
+print("The function is declared like this:")
+print("print('what to print')")
+
+#Day1.12
+print("Hello world!\nHello world!\nHello world!")
+print("Hello" + " " + "Angela")
+print("Hello " + "Angela")
+
+#Day1.2 Debugging Exercise
+#Fix the code below
+
+print("Day 1 - String Manipulation")
+print('String Concatenation is done with the "+" sign.')
+print('e.g. print("Hello " + "world")')
+print("New lines can be created with a blacklash and n.")
+     #The codes above have been fixed
+
+#Day1.2.1 The Python Input Function
+input("What is your name?")
+print("Hello " + input("What is your name?"))
+
+#Day1.3 Inputs Exercise
+input("What's your name?")
+print(len(input("What's your name?")))
+
+#Day1.3.1 Python Variable
+name = input("What is your name?")
+print(name)
+length = len(name)
+print(length)
+
+#Day1.4 Varaibles Exercise
+a = input("a: ")
+b = input("b: ")
+c = b
+d = a
+print("a: " + c)
+print("b: " + d)
+
+#Day1.4.1 Varaible Naming
+user_name =  input("What is your name?")
+
+#Day 1 Final Project "Band Name Generator"
+
+print("Welcome to band name geneartor")
+City =input("Which city did you grow up in?\n")
+Pet = input("What's the name of your pet?\n")
+print("Your band name could be " + city + " " + pet)
